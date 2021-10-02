e7145dbcd906f777359d1251cb2b4756b2c853a4
Your Name <you@example.com>
2021-10-02 23:34:34 +0530

    First commit for call back hell task

---
+++ script.js
@@ -0,0 +1,35 @@
+setTimeout(() => {
+    setTimeout(() => {
+        setTimeout(() => {
+            setTimeout(() => {
+                setTimeout(() => {
+                    setTimeout(() => {
+                        setTimeout(() => {
+                            setTimeout(() => {
+                                setTimeout(() => {
+                                    setTimeout(() => {
+                                        setTimeout(() => {
+                                            setTimeout(() => {
+                                            console.log( "Happy Independence Day");
+                                        }, 1000);
+                                        console.log(1);
+                                    }, 1000);
+                                    console.log(2);
+                                }, 1000);
+                                console.log(3);
+                            }, 1000);
+                            console.log(4);
+                        }, 1000);
+                        console.log(5);
+                    }, 1000);
+                    console.log(6);
+                }, 1000);
+                console.log(7);
+            }, 1000);
+            console.log(8);
+        }, 1000);
+        console.log(9);
+    }, 1000);
+        console.log(10);
+    }, 1000);
+});
